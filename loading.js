79a60ff8662717dbe29673e2ee640b2d9d1c112a
musazoain27-source <musazoain27@gmail.com>
2026-08-11 22:53:00 +0500

    Delete loading.js

--- loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="max-w-7xl mx-auto px-4 py-24 flex items-center justify-center min-h-[40vh]">
-      <div className="w-10 h-10 border-2 border-black/10 border-t-brand-gold rounded-full animate-spin" />
-    </div>
-  );
-}
